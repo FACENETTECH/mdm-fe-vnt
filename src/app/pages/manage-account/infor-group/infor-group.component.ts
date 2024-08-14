88aeb5b149ab5f1cee3ad718cddadb5db03893dd
nguyenhuycuong1 <nhclnhc@gmail.com>
2024-08-14 14:24:11 +0700

    update: add + update data authority

--- src/app/pages/manage-account/infor-group/infor-group.component.ts
+++ src/app/pages/manage-account/infor-group/infor-group.component.ts
@@ -1,23 +1,24 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { UserService } from 'src/app/services/manage-user/user.service';
+import { DATA_TYPE } from 'src/app/utils/constrant';
 
 @Component({
   selector: 'app-infor-group',
   templateUrl: './infor-group.component.html',
-  styleUrls: ['./infor-group.component.css']
+  styleUrls: ['./infor-group.component.css'],
 })
 export class InforGroupComponent {
   constructor(private userService: UserService, private toast: ToastrService) {}
-  
+
   @Input() isvisible: boolean = false;
   @Input() group: any;
-  
+
   groupName: string = '';
   groupCode: string = '';
   authorities: {
-    roleCode: string,
-    roleName: string
+    roleCode: string;
+    roleName: string;
   }[] = [];
   assignedRoles: any[] = [];
   listRole: any[] = [];
@@ -30,15 +31,20 @@ export class InforGroupComponent {
   checkGroupCode: string = '';
   checkAuthorities: string = '';
 
+  columnTable: any[] = [];
+  listDataAuthorities: any[] = [];
+
   @Output() isvisibleChange: EventEmitter<boolean> = new EventEmitter();
 
   isvisibleUpdateGroup: boolean = false;
 
   ngOnInit() {
+    this.columnTable = columnTable;
     this.groupCode = this.group.name;
     this.groupName = this.group.description;
     this.getRole();
     this.getGroup(this.groupCode);
+    this.getDataAuthoritiesTable();
   }
 
   checkValid() {
@@ -89,13 +95,19 @@ export class InforGroupComponent {
       this.authorities = res.data.map((o: any) => {
         return {
           roleCode: o.name,
-          roleName: o.description
-        }
-      })
+          roleName: o.description,
+        };
+      });
       console.log(this.authorities);
     } else {
       this.toast.error(res.result.message);
     }
+  }
+
+  getDataAuthoritiesTable() {
+    this.userService.getDataAuthoritiesByRole(this.groupCode).then((res) => {
+      this.listDataAuthorities = res.data;
+    });
   }
 
   submitDemo() {
@@ -113,20 +125,80 @@ export class InforGroupComponent {
     } else {
       this.requestGroups['description'] = this.groupName;
       this.requestGroups['name'] = this.groupCode;
-      this.requestGroups['roles'] = this.authorities.map(o => o.roleCode);
+      this.requestGroups['roles'] = this.authorities.map((o) => o.roleCode);
 
-      let res = await this.userService.updateGroup(
-        this.requestGroups
-      );
+      let res = await this.userService.updateGroup(this.requestGroups);
       if (res.result.ok) {
         this.toast.success(res.result.message);
         this.isvisibleChange.emit(false);
       } else {
         this.toast.error(res.result.message);
       }
+
+      this.userService
+        .updateListDataAuthorities(this.groupCode, this.listDataAuthorities)
+        .then(() => {})
+        .catch((err) => {
+          this.toast.error(err.error.result.message);
+        });
     }
   }
 
   compareFn = (o1: any, o2: any): boolean =>
     o1 && o2 ? o1.roleName === o2.roleName : o1 === o2;
+
+  protected readonly dataType = DATA_TYPE;
 }
+const columnTable = [
+  {
+    id: 88,
+    index: 1,
+    tableName: '',
+    keyName: 'tableName',
+    keyTitle: 'Bảng',
+    isRequired: false,
+    dataType: 2,
+    hasUnit: false,
+    relateTable: null,
+    relateColumn: null,
+    note: null,
+    addition: null,
+    width: '120px',
+    isFixed: false,
+    localCheck: true,
+  },
+  {
+    id: 88,
+    index: 1,
+    tableName: '',
+    keyName: 'columnName',
+    keyTitle: 'Cột',
+    isRequired: false,
+    dataType: 2,
+    hasUnit: false,
+    relateTable: null,
+    relateColumn: null,
+    note: null,
+    addition: null,
+    width: '120px',
+    isFixed: false,
+    localCheck: true,
+  },
+  {
+    id: 88,
+    index: 1,
+    tableName: '',
+    keyName: 'value',
+    keyTitle: 'Giá trị',
+    isRequired: false,
+    dataType: 9,
+    hasUnit: false,
+    relateTable: null,
+    relateColumn: null,
+    note: null,
+    addition: null,
+    width: '120px',
+    isFixed: false,
+    localCheck: true,
+  },
+];
